826f4ec194e02095e0529b72f3d105eb9c257231
takagiy <takagiy.4dev@gmail.com>
2020-10-29 12:58:06 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+.PHONY: rebuild
+rebuild: submodules /etc/nixos/configuration.nix /etc/nixos/hardware-configuration.nix
+	nixos-rebuild switch --upgrade -I nixpkgs=./.submodules/nixpkgs
+
+.PHONY: install
+install: /etc/nixos/configuration.nix /etc/nixos/hardware-configuration.nix
+
+.PHONY: submodules
+submodules:
+	git submodule init
+	git submodule update
+
+.dest/hardware-configuration.nix:
+	mkdir -p .dest
+	if [ -f /etc/nixos/hardware-configuration.nix ]; \
+	then cp /etc/nixos/hardware-configuration.nix $@; \
+	else sudo nixos-generate-config --show-hardware-config > $@; \
+	fi
+
+/etc/nixos/hardware-configuration.nix: .dest/hardware-configuration.nix
+	cp $< $@
+
+/etc/nixos/configuration.nix: etc/nixos/configuration.nix
+	cp $< $@
---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,188 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix = {
+    # Use unstable version of nix.
+    #package = pkgs.nixUnstable;
+
+    # Enable nix flakes.
+    #extraOptions = ''
+    #  experimental-features = nix-command flakes
+    #
+    #'';
+
+    # Build packages in isolated environment.
+    useSandbox = true;
+  };
+
+  # Use custom nixpkgs.
+  # system.autoUpgrade.flags = [ "-I" "nixpkgs=/home/takagiy/Desk/Repos/nixpkgs" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "modapone";
+  #networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
+  programs.nm-applet = {
+    enable = true;
+    indicator = false;
+  };
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "us";
+
+  # Enable Japanese IME.
+  i18n.inputMethod.enabled = "fcitx";
+  i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Asia/Tokyo";
+
+  # Allow to install proprietary or unfree packages.
+  nixpkgs.config.allowUnfree = true;
+
+  # Exporting environment variables.
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+  environment.systemPackages = with pkgs; [
+    # cli #
+    tree
+    ripgrep
+    git
+    wget
+    (neovim.override { vimAlias = true; })
+    killall
+
+    # languages #
+    nodejs
+
+    # desktop #
+    wmderland
+    ly
+
+    # desktop.general #
+    hsetroot
+    rofi
+    termite
+    networkmanagerapplet
+
+    # desktop.applications #
+    google-chrome
+    pavucontrol
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+  services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.naturalScrolling = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.defaultSession = "none+wmderland";
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.windowManager.wmderland.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+
+  # Enable fish-shell.
+  programs.fish.enable = true;
+
+  # Start fish-shell from the entering step of bash.
+  programs.bash.interactiveShellInit = ''
+    [ -z "$NO_EXEC_FISH" ] && exec fish
+  '';
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+  users.users.takagiy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.bash;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+}
